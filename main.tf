87aadabd740c7d0edf11820b8f604cafadb08abd
Ahmed Makhlouf <ahmed.makhlouf@hashicorp.com>
2025-02-25 11:36:45 +0100

    Single Web Server

--- main.tf
+++ main.tf
@@ -1,10 +1,27 @@
 provider "aws" {
   region = "eu-north-1"
 }
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 resource "aws_instance" "example" {
-  ami           = "ami-0989fb15ce71ba39e" #Ubuntu 22.04 Jammy x64
-  instance_type = "t3.micro"
+  ami                    = "ami-0989fb15ce71ba39e" #Ubuntu 22.04 Jammy x64
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
   tags = {
     Name = "terraform-example"
   }
+
+  user_data                   = <<-EOF
+#!/bin/bash
+echo "Hello, World" > index.html
+nohup busybox httpd -f -p 8080 &
+EOF
+  user_data_replace_on_change = true
 }
