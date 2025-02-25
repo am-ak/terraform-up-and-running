7247d73de858c7a5d5aa42fc250e077d0129c51a
Ahmed Makhlouf <ahmed.makhlouf@hashicorp.com>
2025-02-25 12:30:18 +0100

    Configurable Web Server

--- main.tf
+++ main.tf
@@ -1,15 +1,17 @@
 provider "aws" {
   region = "eu-north-1"
 }
+
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
 resource "aws_instance" "example" {
   ami                    = "ami-0989fb15ce71ba39e" #Ubuntu 22.04 Jammy x64
   instance_type          = "t3.micro"
@@ -17,11 +19,16 @@ resource "aws_instance" "example" {
   tags = {
     Name = "terraform-example"
   }
-
   user_data                   = <<-EOF
 #!/bin/bash
 echo "Hello, World" > index.html
-nohup busybox httpd -f -p 8080 &
+nohup busybox httpd -f -p ${var.server_port} &
 EOF
   user_data_replace_on_change = true
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
 }
