3749a92a341d68c5609715aa8927059b8560c8e7
Ahmed Makhlouf <ahmed.makhlouf@hashicorp.com>
2025-02-26 23:40:49 +0100

    Separate outputs and variables

--- main.tf
+++ main.tf
@@ -182,14 +182,3 @@ resource "aws_dynamodb_table" "terraform_locks" {
     type = "S"
   }
 }
-
-variable "server_port" {
-  description = "The port the server will use for HTTP requests"
-  type        = number
-  default     = 8080
-}
-
-output "alb_dns_name" {
-  value       = aws_lb.example.dns_name
-  description = "The domain name of the load balancer"
-}
